360a964546a975552f5e8ac532f579c3c5ea5c3a
朱毅 <2351950143@qq.com>
2024-07-18 19:35:24 +0800

    springFramework-IOC-xml方式的构造器注入

---
+++ src/main/java/com/basic_di/constructor/ConstructorApplication.java
@@ -0,0 +1,11 @@
+package com.basic_di.constructor;
+
+/**
+ * @author ZhuYi
+ * @create 2024/7/18 19:31
+ */
+public class ConstructorApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/basic_di/constructor/bean/Person.java
@@ -0,0 +1,40 @@
+package com.basic_di.constructor.bean;
+
+/**
+ * @author ZhuYi
+ * @create 2024/7/15 18:30
+ */
+//@Component //开启xml注解扫描时才使用
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
